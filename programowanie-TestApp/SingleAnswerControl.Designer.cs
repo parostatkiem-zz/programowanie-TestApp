eb6a222ba1f5e1c1d98733f89942b335ba6c4ff5
Jan Sudczak <jansudczak@gmail.com>
2018-04-25 22:47:30 +0200

    Saving question partly works

---
+++ programowanie-TestApp/SingleAnswerControl.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace programowanie_TestApp
+{
+    partial class SingleAnswerControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxText = new System.Windows.Forms.TextBox();
+            this.checkBoxIsRight = new System.Windows.Forms.CheckBox();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxText
+            // 
+            this.textBoxText.Location = new System.Drawing.Point(4, 4);
+            this.textBoxText.Name = "textBoxText";
+            this.textBoxText.Size = new System.Drawing.Size(223, 20);
+            this.textBoxText.TabIndex = 0;
+            // 
+            // checkBoxIsRight
+            // 
+            this.checkBoxIsRight.AutoSize = true;
+            this.checkBoxIsRight.Location = new System.Drawing.Point(233, 6);
+            this.checkBoxIsRight.Name = "checkBoxIsRight";
+            this.checkBoxIsRight.Size = new System.Drawing.Size(74, 17);
+            this.checkBoxIsRight.TabIndex = 1;
+            this.checkBoxIsRight.Text = "Poprawna";
+            this.checkBoxIsRight.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackgroundImage = global::programowanie_TestApp.Properties.Resources.if_Delete_1493279;
+            this.buttonDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonDelete.Location = new System.Drawing.Point(319, 3);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(33, 25);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // SingleAnswer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.checkBoxIsRight);
+            this.Controls.Add(this.textBoxText);
+            this.Name = "SingleAnswer";
+            this.Size = new System.Drawing.Size(355, 31);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxText;
+        private System.Windows.Forms.CheckBox checkBoxIsRight;
+        private System.Windows.Forms.Button buttonDelete;
+    }
+}
---
+++ programowanie-TestApp/SingleAnswerControl.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace programowanie_TestApp
+{
+    public partial class SingleAnswerControl : UserControl
+    {
+        public SingleAnswerControl()
+        {
+            InitializeComponent();
+        }
+
+        public SingleAnswerControl(string _text, bool _isRight)
+        {
+            InitializeComponent();
+            this.TypedText = _text;
+            this.IsRight = _isRight;
+            
+        }
+
+        public string TypedText
+        {
+            get
+            {
+                return textBoxText.Text;
+            }
+            set
+            {
+                //dodać sprawdzanie warunków
+                if (value == null) value = "";
+                textBoxText.Text = value;
+            }
+        }
+
+        public bool IsRight
+        {
+            get
+            {
+                return checkBoxIsRight.Checked;
+            }
+            set
+            {
+                checkBoxIsRight.Checked = value;
+            }
+            //dodać setter
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
